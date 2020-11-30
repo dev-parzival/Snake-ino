8afe97f71e000d443d3dd10f89b579c7def259a7
Malte Linke <malte.linke.ml@gmail.com>
2020-11-30 15:14:36 +0100

    Projektdateien hinzufügen.

---
+++ Snake-ino/Snake-ino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Snake_ino.ino
+ Created:	30.11.2020 15:08:47
+ Author:	parzival
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
